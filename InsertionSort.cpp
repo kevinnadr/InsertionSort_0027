3d808edf4e17cb7578aeb3900bfd4e79f411c99d
Kevin Adrian <kevin19305.ib@gmail.com>
2025-03-18 11:32:30 +0700

    membuat void input

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -3,3 +3,22 @@ using namespace std;
 
 int arr[20]; // Membuat Array dengan panjang data 20
 int n; // Membuat variable inputan n
+
+void input()
+{ // Procedure input
+    while (true)
+    {
+        cout << "Masukkan jumlah data pada array :"; // membuat inputan jumlagh elemen array
+        cin >> n; // memanggil variable inputan n
+
+        if (n <=20)
+        {
+        break;
+        }
+        else
+        {
+            cout << "\nArray yang anda masukkan maksimal 20 elemen. \n"; // menampilkan pesan jika data lebih dari 20
+        }
+    }
+    
+}
